1325080c600a9a7b1fc75c41972324480ed23c35
ReshiAdavan <reshiadavan27@gmail,com>
2022-07-18 11:42:48 -0400

    Adding server-side changes for UMA

---
+++ Server-side/Server-side/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TUT2022.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
